67c4e5b24e535375ceed420ad567dbe63854c5c1
Dylan <Dylan@192.168.1.43>
2018-04-18 11:22:47 +0200

    The beginning of methods writing for RAM class
    - getByte()
    - setByte()
    - getWord()
    - cleanMemory()
    
    With the Java Doc.

--- src/simulateurARM/Ram.java
+++ src/simulateurARM/Ram.java
@@ -1,91 +1,108 @@
 package simulateurARM;
 
 public class Ram implements Memory {
-
-	// TODO write javadoc comment
 	/**
-	 * 
+	 * The default size of the memory
 	 */
 	public static final int DEFAULT_SIZE = 10000;
-
-	// TODO write javadoc comment
+	
 	/**
-	 * 
+	 * A table of byte that represent the memory
 	 */
 	private byte[] memory;
 
-
-	// TODO write javadoc comment
 	/**
-	 * 
+	 * A constructor of a RAM with a specified size
+	 * @param size The size of the RAM
 	 */
 	public Ram(int size) {
 		this.memory = new byte[size];
 	}
-
-	// TODO write javadoc comment
+	
 	/**
-	 * 
+	 * A constructor of a RAM with the default size
+	 * @param size The size of the RAM
 	 */
 	public Ram() {
 		this.memory = new byte[DEFAULT_SIZE];
 	}
-
-	// TODO write javadoc comment
+	
 	/**
-	 * 
+	 * @param myAddress The address where we get the byte
+	 * @return A byte corresponds to the address specified
 	 */
 	public byte getByte(Address myAddress) throws InvalidMemoryAddressException{
-		return 0;
+		if((myAddress.getAddress()<0)||(myAddress.getAddress()>=memory.length)){
+			throw new InvalidMemoryAddressException();
+		}
+		return this.memory[myAddress.getAddress()];
 	}
-
-	// TODO write javadoc comment
+	
 	/**
-	 * 
+	 * @param myAddress The address where we set the byte
+	 * @param myByte 
+	 * @return The byte set in the memory
 	 */
 	public byte setByte(Address myAddress, byte myByte) throws InvalidMemoryAddressException {
-		return 0;
-
+		if((myAddress.getAddress()<0)||(myAddress.getAddress()>=memory.length)){
+			throw new InvalidMemoryAddressException();
+		}
+		this.memory[myAddress.getAddress()] = myByte;
+		return this.memory[myAddress.getAddress()];
 	}
 	
-	// TODO write javadoc comment
 	/**
-	 * 
+	 * @param myAddress
+	 * @return a Word 
 	 */
 	public short getWord(Address myAddress) throws InvalidMemoryAddressException{
+		if((myAddress.getAddress()<0)||(myAddress.getAddress()>=memory.length)){
+			throw new InvalidMemoryAddressException();
+		}
+		//int myWord = this.memory[myAddress.getAddress()+3] << 24+this.memory[myAddress.getAddress()+2] << 16+this.memory[myAddress.getAddress()+1] << 8+this.memory[myAddress.getAddress()];
 		return 0;
 	}
-
-	// TODO write javadoc comment
+	
 	/**
-	 * 
+	 * @param myAddress
+	 * @param myWord
+	 * @return a Word 
 	 */
 	public short setWord(Address myAddress, short myWord) throws InvalidMemoryAddressException {
+		if((myAddress.getAddress()<0)||(myAddress.getAddress()>=memory.length)){
+			throw new InvalidMemoryAddressException();
+		}
 		return 0;
-
+		
 	}
 	
-	// TODO write javadoc comment
 	/**
-	 * 
+	 * @param
 	 */
 	public int get(Address myAddress) throws InvalidMemoryAddressException{
+		if((myAddress.getAddress()<0)||(myAddress.getAddress()>=memory.length)){
+			throw new InvalidMemoryAddressException();
+		}
 		return 0;
 	}
 	
-	// TODO write javadoc comment
 	/**
-	 * 
+	 * @param
 	 */
 	public int set(Address myAddress, int value) throws InvalidMemoryAddressException{
+		if((myAddress.getAddress()<0)||(myAddress.getAddress()>=memory.length)){
+			throw new InvalidMemoryAddressException();
+		}
 		return 0;
 	}
 	
-	// TODO write javadoc comment
 	/**
-	 * 
+	 * Put all the byte of the memory to 0.
 	 */
 	public void cleanMemory(){
+		for(int i=0; i < memory.length; i++ ) {
+			this.memory[i]=0;
+		}
+	} 
 
-	}
-}
+} 
