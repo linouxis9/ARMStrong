689c02773fc61afc996f8d94e37b10d0ffb1b7e6
Valentin D'Emmanuele <valentin.d-emmanuele@etu.univ-grenoble-alpes.fr>
2018-04-29 17:22:01 +0200

    Add ProgramTests TestCase and fix some regexp bugs found thanks to it!
    * Fix some TokenType's regular expressions

--- src/simulator/Token.java
+++ src/simulator/Token.java
@@ -32,5 +32,33 @@ public class Token {
 				return this.value;
 		}
 	}
+
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((token == null) ? 0 : token.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Token other = (Token) obj;
+		if (token != other.token)
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
---
+++ src/test/ProgramTests.java
@@ -0,0 +1,85 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+import simulator.Program;
+import simulator.Token;
+import simulator.TokenType;
+
+public class ProgramTests {
+    @Test
+	public void testLexer() {
+		List<Token> tokens = Program.lexer("mov r4,r4");
+		Set<Token> expected = new HashSet<Token>() {{
+		    add(new Token(TokenType.OPERATION,"mov"));
+		    add(new Token(TokenType.REGISTER,"r4"));
+		    add(new Token(TokenType.COMMA,","));
+		    add(new Token(TokenType.REGISTER,"r4"));
+		}};
+		assertTrue(expected.containsAll(tokens));
+		
+		tokens = Program.lexer("mov r4,#4");
+		expected = new HashSet<Token>() {{
+		    add(new Token(TokenType.OPERATION,"mov"));
+		    add(new Token(TokenType.REGISTER,"r4"));
+		    add(new Token(TokenType.COMMA,","));
+		    add(new Token(TokenType.HASH,"#4"));
+		}};
+		
+		tokens = Program.lexer("mov r4,#+4");
+		expected = new HashSet<Token>() {{
+		    add(new Token(TokenType.OPERATION,"mov"));
+		    add(new Token(TokenType.REGISTER,"r4"));
+		    add(new Token(TokenType.COMMA,","));
+		    add(new Token(TokenType.HASH,"#+4"));
+		}};
+		
+		tokens = Program.lexer("mov r4,#-4");
+		expected = new HashSet<Token>() {{
+		    add(new Token(TokenType.OPERATION,"mov"));
+		    add(new Token(TokenType.REGISTER,"r4"));
+		    add(new Token(TokenType.COMMA,","));
+		    add(new Token(TokenType.HASH,"#-4"));
+		}};
+			
+		tokens = Program.lexer("b coucou");
+		expected = new HashSet<Token>() {{
+		    add(new Token(TokenType.OPERATION,"b"));
+		    add(new Token(TokenType.IDENTIFIER,"coucou"));
+		}};
+		
+		tokens = Program.lexer("coucou:b coucou");
+		expected = new HashSet<Token>() {{
+			add(new Token(TokenType.LABEL,"coucou:"));
+		    add(new Token(TokenType.OPERATION,"b"));
+		    add(new Token(TokenType.IDENTIFIER,"coucou"));
+		}};
+		assertTrue(expected.containsAll(tokens));
+		
+		tokens = Program.lexer("coucou:ldr r4,[r0]");
+		expected = new HashSet<Token>() {{
+			add(new Token(TokenType.LABEL,"coucou:"));
+		    add(new Token(TokenType.OPERATION,"ldr"));
+		    add(new Token(TokenType.REGISTER,"r4"));
+		    add(new Token(TokenType.COMMA,","));
+		    add(new Token(TokenType.OFFSET,"[r0]"));
+		}};
+		
+		tokens = Program.lexer("@coucou:ldr r4,[r0]");
+		expected = new HashSet<Token>() {{
+			add(new Token(TokenType.COMMENT,"@coucou:ldr r4,[r0]"));
+		}};
+		
+		tokens = Program.lexer(".align 4");
+		expected = new HashSet<Token>() {{
+			add(new Token(TokenType.DIRECTIVE,".align 4"));
+		}};
+		assertTrue(expected.containsAll(tokens));
+	}
+}
