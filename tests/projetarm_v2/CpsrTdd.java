94e32e1677f8736c6e774f0524589261a3e38de3
Dylan <Dylan.Fraisse@etu.univ-grenoble-alpes.fr>
2019-01-03 18:46:42 +0100

    Red setC setV setQ

--- tests/projetarm_v2/CpsrTdd.java
+++ tests/projetarm_v2/CpsrTdd.java
@@ -26,6 +26,10 @@ class CpsrTdd {
 		this.simulator.setProgram("mov r0,#20; mov r1,#15; cmp r0,r1");
 		this.simulator.run();
 		assertFalse(this.simulator.getN());
+	}
+	
+	@Test
+	public void testSetN() {
 		this.cpsr.setN(false);
 		assertFalse(this.simulator.getN());
 		this.cpsr.setN(true);
@@ -33,15 +37,14 @@ class CpsrTdd {
 	}
 	
 	@Test
-	public void testNnoValue() {
-		assertFalse(this.simulator.getN());
-	}
-	
-	@Test
 	public void testZ() {
 		this.simulator.setProgram("mov r0,#20; mov r1,#20; cmp r0,r1");
 		this.simulator.run();
 		assertTrue(this.simulator.getZ());
+	}
+	
+	@Test
+	public void testSetZ() {
 		this.cpsr.setZ(false);
 		assertFalse(this.simulator.getZ());
 		this.cpsr.setZ(true);
@@ -53,8 +56,14 @@ class CpsrTdd {
 		this.simulator.setProgram("mov r0,#1; mov r1,#1; cmp r0,r1");
 		this.simulator.run();
 		assertTrue(this.simulator.getC());
-		//this.cpsr.setC(false);
-		//assertFalse(this.simulator.getC());
+	}
+	
+	@Test
+	public void testSetC(){
+		this.cpsr.setC(true);
+		assertTrue(this.simulator.getC());
+		this.cpsr.setC(false);
+		assertFalse(this.simulator.getC());
 	}
 	
 	@Test
@@ -62,8 +71,14 @@ class CpsrTdd {
 		this.simulator.setProgram("mov r0, #0x80000000; mov  r1, #0x00000001; subs r2, r0, r1");
 		this.simulator.run();
 		assertTrue(this.simulator.getV());
-		//this.cpsr.setV(false);
-		//assertFalse(this.simulator.getV());
+	}
+	
+	@Test
+	public void testSetV(){
+		this.cpsr.setV(true);
+		assertTrue(this.simulator.getV());
+		this.cpsr.setV(false);
+		assertFalse(this.simulator.getV());
 	}
 	
 	@Test
@@ -71,8 +86,13 @@ class CpsrTdd {
 		this.simulator.setProgram("mov r2,#0x70000000; qadd r3,r2,r2");
 		this.simulator.run();
 		assertTrue(this.simulator.getQ());
-		//this.cpsr.setQ(false);
-		//assertFalse(this.simulator.getQ());
 	}
 
+	@Test
+	public void testSetQ() {
+		this.cpsr.setQ(false);
+		assertFalse(this.simulator.getQ());
+		this.cpsr.setQ(true);
+		assertTrue(this.simulator.getQ());
+	}
 }
