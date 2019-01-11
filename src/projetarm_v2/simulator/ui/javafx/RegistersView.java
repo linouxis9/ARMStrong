463e3834234dc79a1e28006c7f06773bec46976d
Dylan Fraisse <dylan.fraisse@etu.univ-grenoble-alpes.fr>
2019-01-11 22:51:28 +0100

    Finish registerView (but we can improve it)

--- src/projetarm_v2/simulator/ui/javafx/RegistersView.java
+++ src/projetarm_v2/simulator/ui/javafx/RegistersView.java
@@ -10,6 +10,7 @@ import javafx.scene.text.Text;
 import projetarm_v2.simulator.boilerplate.ArmSimulator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.dockfx.DockNode;
 import org.dockfx.DockPane;
@@ -19,8 +20,9 @@ public class RegistersView {
     TabPane mainPane;
     DockNode dockNode;
     Image dockImage;
-    ArrayList<Text> registersLabel;
-    ArrayList<Text> registersValues;
+    ArrayList<Text> registersHex;
+    ArrayList<Text> registersSigHex;
+    ArrayList<Text> registersDec;
     ArmSimulator simulator;
     Pane hexPane;
     Pane sigHexPane;
@@ -32,6 +34,7 @@ public class RegistersView {
     public RegistersView(ArmSimulator simulator){
        // dockImage = new Image(Gui.class.getResource("docknode.png").toExternalForm());
     	this.simulator = simulator;
+		
         this.hexPane = new Pane();
         this.sigHexPane = new Pane();
         this.decPane = new Pane();
@@ -45,40 +48,77 @@ public class RegistersView {
         this.dockNode = new DockNode(mainPane, "Register View", new ImageView(dockImage));
         this.dockNode.setPrefSize(300, 100);
         
-        this.registersValues = new ArrayList<Text>();
+        this.registersHex = new ArrayList<Text>();
+        this.registersSigHex = new ArrayList<Text>();
+        this.registersDec = new ArrayList<Text>();
 
         updateRegisters();
         
     }
     
     public void updateRegisters(){
-    	for(int i = 0; i<this.registersValues.size(); i++){
-    		this.hexPane.getChildren().remove(this.registersValues.get(i));
-    		this.sigHexPane.getChildren().remove(this.registersValues.get(i));
-    		this.decPane.getChildren().remove(this.registersValues.get(i));
+    	for(int i = 0; i<this.registersHex.size(); i++){
+    		this.hexPane.getChildren().remove(this.registersHex.get(i));
+    		this.sigHexPane.getChildren().remove(this.registersSigHex.get(i));
+    		this.decPane.getChildren().remove(this.registersDec.get(i));
         }
-    	this.registersValues.clear();
+    	this.registersHex.clear();
+    	this.registersSigHex.clear();
+    	this.registersDec.clear();
     	
     	for (int i = 0; i < 16; i++) {
-    		Text register = new Text(10,20*(i+1), String.format("R%d = 0x%x", i,simulator.getRegisterValue(i)));
-  		   	this.registersValues.add(register);
+    		Text registerHex = new Text(10,20*(i+1), String.format("R%d = 0x%x", i,simulator.getRegisterValue(i)));
+  		   	this.registersHex.add(registerHex);
+  		   	
+		   	Text registerDec = new Text(10,20*(i+1), String.format("R%d = %d", i, simulator.getRegisterValue(i)));
+  		   	this.registersDec.add(registerDec);
+  		   	
+  		   	int valueDec = simulator.getRegisterValue(i);
+  		   	int contentSigHex =  (valueDec > 32767) ? valueDec - 65536 : valueDec;
+  		  	Text registerSigHex = new Text(10,20*(i+1), String.format("R%d = 0x%x", i, contentSigHex));
+  		  	this.registersSigHex.add(registerSigHex);
   		}
          
-         Text flagN = new Text(10,20*(17), String.format("N : %s", simulator.getN()));
-         Text flagZ = new Text(10,20*(18), String.format("Z : %s", simulator.getZ()));
-         Text flagC = new Text(10,20*(19), String.format("C : %s", simulator.getC()));
-         Text flagV = new Text(10,20*(20), String.format("V : %s", simulator.getV()));
-         Text flagQ = new Text(10,20*(21), String.format("Q : %s", simulator.getQ()));
+         Text flagnHex = new Text(10,20*(17), String.format("N : %s", simulator.getN()));
+         Text flagzHex = new Text(10,20*(18), String.format("Z : %s", simulator.getZ()));
+         Text flagcHex = new Text(10,20*(19), String.format("C : %s", simulator.getC()));
+         Text flagvHex = new Text(10,20*(20), String.format("V : %s", simulator.getV()));
+         Text flagqHex = new Text(10,20*(21), String.format("Q : %s", simulator.getQ()));
          
-         this.registersValues.add(flagN);
-         this.registersValues.add(flagZ);
-         this.registersValues.add(flagC);
-         this.registersValues.add(flagV);
-         this.registersValues.add(flagQ);
+         ArrayList<Text> flagCPSRHex = new ArrayList<Text>();
+         flagCPSRHex.addAll(Arrays.asList(flagnHex,flagzHex,flagcHex,flagvHex,flagqHex));
          
-         this.hexPane.getChildren().addAll(this.registersValues);
+         this.registersHex.addAll(flagCPSRHex);
+         
+         Text flagnSigHex = new Text(10,20*(17), String.format("N : %s", simulator.getN()));
+         Text flagzSigHex = new Text(10,20*(18), String.format("Z : %s", simulator.getZ()));
+         Text flagcSigHex = new Text(10,20*(19), String.format("C : %s", simulator.getC()));
+         Text flagvSigHex = new Text(10,20*(20), String.format("V : %s", simulator.getV()));
+         Text flagqSigHex = new Text(10,20*(21), String.format("Q : %s", simulator.getQ()));
+         
+         ArrayList<Text> flagCPSRSigHex = new ArrayList<Text>();
+         flagCPSRSigHex.addAll(Arrays.asList(flagnSigHex,flagzSigHex,flagcSigHex,flagvSigHex,flagqSigHex));
+
+         this.registersSigHex.addAll(flagCPSRSigHex);
+         
+         Text flagnDec = new Text(10,20*(17), String.format("N : %s", simulator.getN()));
+         Text flagzDec = new Text(10,20*(18), String.format("Z : %s", simulator.getZ()));
+         Text flagcDec = new Text(10,20*(19), String.format("C : %s", simulator.getC()));
+         Text flagvDec = new Text(10,20*(20), String.format("V : %s", simulator.getV()));
+         Text flagqDec = new Text(10,20*(21), String.format("Q : %s", simulator.getQ()));
+         
+         ArrayList<Text> flagCPSRDec = new ArrayList<Text>();
+         flagCPSRDec.addAll(Arrays.asList(flagnDec,flagzDec,flagcDec,flagvDec,flagqDec));
+
+         this.registersDec.addAll(flagCPSRDec);
+         
+         this.hexPane.getChildren().addAll(this.registersHex);
+         this.sigHexPane.getChildren().addAll(this.registersSigHex);
+         this.decPane.getChildren().addAll(this.registersDec);
          
          this.hexTab.setContent(this.hexPane);
+         this.sigHexTab.setContent(this.sigHexPane);
+         this.decTab.setContent(this.decPane);
     }
 
     DockNode getNode(){
