cf8ad9964ff551cf493449199b90a85b8c55d96e
Gilles Mertens <gilles.mertens@etu.univ-grenoble-alpes.fr>
2019-03-27 12:43:29 +0100

    more 8SEGMENTS!!

--- src/projetarm_v2/simulator/ui/javafx/EightSegmentDisplay.java
+++ src/projetarm_v2/simulator/ui/javafx/EightSegmentDisplay.java
@@ -3,9 +3,10 @@ package projetarm_v2.simulator.ui.javafx;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 import org.dockfx.DockNode;
 import projetarm_v2.simulator.boilerplate.ArmSimulator;
 import projetarm_v2.simulator.core.io.IO8Segment;
@@ -13,42 +14,49 @@ import projetarm_v2.simulator.core.io.IO8Segment;
 import java.util.ArrayList;
 
 public class EightSegmentDisplay {
-    Pane mainPane;
+    HBox mainPane;
     DockNode dockNode;
 
     Text address;
-    ArrayList<ImageView> images;
-    IO8Segment segment;
+    ArrayList<ArrayList<ImageView>> images;
+    ArrayList<IO8Segment> segments;
 
     private ArmSimulator simulator;
 
     public EightSegmentDisplay(ArmSimulator simulator){
 
-        this.mainPane = new AnchorPane();
+        this.mainPane = new HBox();
         images = new ArrayList<>();
+        segments = new ArrayList<>();
 
-        this.mainPane.getChildren().add(new ImageView(new Image(getClass().getResource("/resources/8_segment/8seg_base.png").toExternalForm())));
-        for (int i=0; i<8; i++){
-            images.add(new ImageView(new Image(getClass().getResource("/resources/8_segment/8seg_" + (i+1) + ".png").toExternalForm())));
-            this.mainPane.getChildren().add(images.get(i));
-            images.get(i).setVisible(false);
+        for(int j = 0; j<15; j++) {
+            VBox currentVBox = new VBox();
+            this.mainPane.getChildren().add(currentVBox);
+            AnchorPane imagesContainer = new AnchorPane();
+            currentVBox.getChildren().add(imagesContainer);
+            imagesContainer.getChildren().add(new ImageView(new Image(getClass().getResource("/resources/8_segment/8seg_base.png").toExternalForm())));
+            images.add(new ArrayList<>());
+            for (int i = 0; i < 8; i++) {
+                ImageView currentImage = new ImageView(new Image(getClass().getResource("/resources/8_segment/8seg_" + (i + 1) + ".png").toExternalForm()));
+                imagesContainer.getChildren().add(currentImage);
+                images.get(j).add(currentImage);
+                currentImage.setVisible(false);
+            }
+            segments.add(simulator.newIO8Segment());
+            currentVBox.getChildren().add(new Text("0x" + Long.toHexString(segments.get(j).getSegment(0).getPortAddress())));
         }
 
         this.dockNode = new DockNode(mainPane, "8 segment view");
-
         dockNode.setPrefSize(460,666);
-        segment = simulator.newIO8Segment();
-
-        address = new Text(""+segment.getSegment(1).getPortAddress());
-        mainPane.getChildren().add(address);
 
     }
 
     public void refresh() {
-        for (int i=0; i<8; i++){
-            images.get(i).setVisible(segment.getSegmentState(i));
+        for (int j=0; j<15; j++){
+            for (int i=0; i<8; i++){
+                images.get(j).get(i).setVisible(segments.get(j).getSegmentState(i));
+            }
         }
-
     }
 
     public DockNode getNode() {
