72e0b475d121ef624fed0402412e577d3acf879e
Valentin <valentin@devling.xyz>
2020-01-23 21:05:55 +0100

    Fix a visual issue with the simulation mode

--- src/projetarm_v2/simulator/core/Preprocessor.java
+++ src/projetarm_v2/simulator/core/Preprocessor.java
@@ -20,8 +20,9 @@ public class Preprocessor {
 	public static final String LABEL_PATTERN = START_OF_LINE + "(([a-zA-Z]|[0-9])*):";
 	public static final Pattern labelPattern = Pattern.compile(LABEL_PATTERN);
 	public static final Pattern emptyLabelPattern = Pattern.compile(LABEL_PATTERN + "\\s*$");
+	public static final Pattern multiLinesComments = Pattern.compile("(?s)(\\/\\*.*?\\*\\/)", Pattern.DOTALL);
 	private static final Pattern equPattern = Pattern.compile(START_OF_LINE + "\\.equ +(.*),(.*)");
-	
+
 	public static String pass1(String assembly) {
 		Matcher matcher = equPattern.matcher(assembly);
 		
@@ -31,23 +32,26 @@ public class Preprocessor {
 		}
 
 		matcher = labelPattern.matcher(assembly);
-
 		while (matcher.find()) {
-			assembly = assembly.replaceAll(START_OF_LINE + matcher.group(1), escapeDigit(matcher.group(1)));
-			assembly = assembly.replaceAll("=" + matcher.group(1), "=" + escapeDigit(matcher.group(1)));
+			assembly = assembly.replaceAll(matcher.group(1), escapeDigit(matcher.group(1)));
+		}
+
+		matcher = multiLinesComments.matcher(assembly);
+		while (matcher.find()) {
+			assembly = assembly.replace(matcher.group(1), ";".repeat(matcher.group(1).split("\\R").length-1));
 		}
 
 		assembly = assembly
+				.replaceAll("@.*", "")
 				.replaceAll(START_OF_LINE + "\\.breakpoint", "blx #" + CpuBreakpoint.ROUTINE_ADDRESS)
 				.replaceAll(START_OF_LINE + "\\.clear", "blx #" + CpuConsoleClear.ROUTINE_ADDRESS)
 				.replaceAll(START_OF_LINE + "\\.stop", ".word 0")
 				.replaceAll(START_OF_LINE + "\\.end", ".word 0")
-				.replaceAll(START_OF_LINE +"\\.equ +.*", "")
-				.replaceAll("@.*", "");
+				.replaceAll(START_OF_LINE +"\\.equ +.*", "");
+
 
 		return assembly.replaceAll("\\R",";");
 	}
-
 
 	private static String escapeDigit(String group) {
 		StringBuilder builder = new StringBuilder();
@@ -65,6 +69,8 @@ public class Preprocessor {
 				case '7': builder.append("Se"); break;
 				case '8': builder.append("E"); break;
 				case '9': builder.append("N"); break;
+				case '_': builder.append("Und"); break;
+				case '-': builder.append("Min"); break;
 			}
 		}
 		
